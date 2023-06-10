8e38b2094433782735ce997243d989b0b338df51
TanKhang <doank3442@gmail.com>
2023-06-10 13:12:10 +0700

    add search user by elasticsearch

---
+++ src/main/java/com/quangduong/SE330backend/entity/sql/LabelEntity.java
@@ -0,0 +1,56 @@
+package com.quangduong.SE330backend.entity.sql;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "label")
+public class LabelEntity extends BaseEntity {
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "color")
+    private String color;
+
+    @ManyToOne
+    @JoinColumn(name = "board_id")
+    private BoardEntity board;
+
+    @OneToMany(mappedBy = "value", cascade = CascadeType.ALL)
+    private List<LabelAttributeEntity> labelAttributes = new ArrayList<>();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public BoardEntity getBoard() {
+        return board;
+    }
+
+    public void setBoard(BoardEntity board) {
+        this.board = board;
+    }
+
+    public List<LabelAttributeEntity> getLabelAttributes() {
+        return labelAttributes;
+    }
+
+    public void setLabelAttributes(List<LabelAttributeEntity> labelAttributes) {
+        this.labelAttributes = labelAttributes;
+    }
+}
---
+++ src/main/java/com/quangduong/SE330backend/entity/sql/UserEntity.java
@@ -0,0 +1,133 @@
+package com.quangduong.SE330backend.entity.sql;
+
+import com.quangduong.SE330backend.constant.UserStatus;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+public class UserEntity extends BaseEntity {
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "displayname")
+    private String displayName;
+
+    @Column(name = "givenname")
+    private String givenName;
+
+    @Column(name = "familyname")
+    private String familyName;
+
+    @Column(name = "photourl")
+    private String photoUrl;
+
+    @Column(name = "status")
+    private UserStatus status;
+
+    @OneToMany(mappedBy = "admin")
+    private List<BoardEntity> ownerBoards = new ArrayList<>();
+
+    @ManyToMany(mappedBy = "members")
+    private List<BoardEntity> boards = new ArrayList<>();
+
+    @ManyToMany(mappedBy = "members")
+    private List<TableEntity> tables = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user")
+    private List<TaskEntity> tasks = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user")
+    private List<NotificationEntity> notifications = new ArrayList<>();
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getGivenName() {
+        return givenName;
+    }
+
+    public void setGivenName(String givenName) {
+        this.givenName = givenName;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public List<BoardEntity> getOwnerBoards() {
+        return ownerBoards;
+    }
+
+    public void setOwnerBoards(List<BoardEntity> ownerBoards) {
+        this.ownerBoards = ownerBoards;
+    }
+
+    public List<BoardEntity> getBoards() {
+        return boards;
+    }
+
+    public void setBoards(List<BoardEntity> boards) {
+        this.boards = boards;
+    }
+
+    public List<TableEntity> getTables() {
+        return tables;
+    }
+
+    public void setTables(List<TableEntity> tables) {
+        this.tables = tables;
+    }
+
+    public List<TaskEntity> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<TaskEntity> tasks) {
+        this.tasks = tasks;
+    }
+
+    public UserStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(UserStatus status) {
+        this.status = status;
+    }
+
+    public List<NotificationEntity> getNotifications() {
+        return notifications;
+    }
+
+    public void setNotifications(List<NotificationEntity> notifications) {
+        this.notifications = notifications;
+    }
+}
